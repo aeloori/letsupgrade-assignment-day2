405de4368695ca475a30e7dc7b2c8de5fce6c719
aeloori <71598984+aeloori@users.noreply.github.com>
2021-09-04 14:53:29 +0530

    Add files via upload

---
+++ prime or not.py
@@ -0,0 +1,11 @@
+num=int(input("enter one number"))
+flag=False
+if num>1:
+    for i in range(2,num):
+        if (num%i)==0:
+            flag=True
+            break
+if flag:
+    print(f"{num}\tis not a prime number")
+else:
+    print(f"{num}\tis a prime number")
